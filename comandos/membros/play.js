9bfd3c1def542a45708e24265206bb20b1851b5e
DanielVitor31 <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2023-12-06 20:23:01 -0300

    Update play.js

--- comandos/membros/play.js
+++ comandos/membros/play.js
@@ -4,9 +4,6 @@ const { joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnectio
 const yts = require('yt-search');
 
 
-//Aviso já esta desbloqueado
-const textoTempoExpirado = `❌ Tempo expirado use o comando novamente!`;
-const avisoTempoExpirado = Aviso(textoTempoExpirado);
 
 module.exports = {
   name: "play",
