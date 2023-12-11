d47b960a7dad4f6b9e1247794678df97f5cae695
DanielVitor31 <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2023-12-11 12:20:58 -0300

    Update functions.js

--- estruturas/functions/functions.js
+++ estruturas/functions/functions.js
@@ -155,63 +155,7 @@ module.exports = {
   },
 
 
-  tratarInfosYoutube: async function (video, interaction, voiceChannel) {
 
-    const thumbDoVideoMax = `https://img.youtube.com/vi/${video.videoId}/maxresdefault.jpg`
-
-    const membro = interaction.user
-    const guild = interaction.guild
-
-    const tag = membro.discriminator === '0' ? "Indisponível (conta criada recentemente)" : `#${membro.discriminator}`
-
-    let solicitado = null;
-
-
-    if (voiceChannel) {
-      solicitado = {
-        membro: {
-          nome: membro.username,
-          tag: tag,
-          id: membro.id,
-        },
-        servidor: {
-          nome: guild.name,
-          id: guild.id,
-          canal: {
-            nome: voiceChannel.name,
-            id: voiceChannel.id,
-          }
-        },
-      }
-    }
-
-
-
-
-
-
-    const videosTratados = {
-      type: 'video',
-      titulo: video.title || "O título possui caracteres, símbolos ou emojis que não posso reproduzir.",
-      description: video.description || "Indisponível",
-      url: video.url || "Indisponível",
-      solocitadoPor: solicitado || "Indisponível",
-      id: video.videoId || "Indisponível",
-      segundos: video.seconds || "Indisponível",
-      tempo: video.timestamp || "Indisponível",
-      duration: video.duration || "Indisponível",
-      views: video.views ? video.views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") : "Indisponível",
-      upload: video.ago ? await converterDataParaPortugues(video.ago) : "Indisponível",
-      imagem: video.image ? thumbDoVideoMax : Midia.Gif.VideoSemImagem,
-      thumb: video.thumbnail ? thumbDoVideoMax : Midia.Gif.VideoSemImagem,
-      canalDoYoutube: video.author || "Indisponível",
-    };
-
-
-
-
-    return videosTratados
-  },
 
 
   // Função para notificar o desenvolvedor por mensagem direta (DM)
